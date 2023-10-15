0c21de4437d35e640a8edb79ef32e959dfbc0049
Aliya15 <aliyuwe4ka@gmail.com>
2023-10-15 16:57:55 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {PokemonsPageModule} from './pages/pokemons-page/pokemons-page.module';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    PokemonsPageModule,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+}
